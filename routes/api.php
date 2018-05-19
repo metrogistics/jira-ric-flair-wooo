efa16948ccedc5ae27c73f8e428c82e62dc71a60
rwfwcb <rwfwcb@mail.missouri.edu>
2018-05-19 13:21:53 -0500

    Log webhook input from JIRA

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,42 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/webhooks/ric-flair/{api_token}', function(Request $request, $api_token) {
+
+   if ($api_token == 'mgRgNQibyUbMReoNcTmJkDRrFzKNmMnsirJoCcyGr8GfufEf'){
+
+       \Illuminate\Support\Facades\Log::info('GET REQUEST.');
+
+       \Illuminate\Support\Facades\Log::info('JIRA request input: ' . print_r($request->input(), true));
+
+       return response()->json(['yell' => 'WOOOOO!']);
+
+   } else {
+
+       \Illuminate\Support\Facades\Log::error('Invalid API token.');
+
+       return response()->json(['success' => false, 'message' => 'Invalid API token.'], 401);
+
+   }
+});
+
+Route::post('/webhooks/ric-flair/{api_token}', function(Request $request, $api_token) {
+
+    if ($api_token == 'mgRgNQibyUbMReoNcTmJkDRrFzKNmMnsirJoCcyGr8GfufEf'){
+
+        \Illuminate\Support\Facades\Log::info('POST REQUEST.');
+
+        \Illuminate\Support\Facades\Log::info('JIRA request input: ' . print_r($request->input(), true));
+
+        return response()->json(['yell' => 'WOOOOO!']);
+
+    } else {
+
+        \Illuminate\Support\Facades\Log::error('Invalid API token.');
+
+        return response()->json(['success' => false, 'message' => 'Invalid API token.'], 401);
+
+    }
+});
