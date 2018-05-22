d816b11c52c4052900b27ccbb5cc7ece7f017e14
rwfwcb <rwfwcb@mail.missouri.edu>
2018-05-21 22:04:38 -0500

    scheduled command to reset the counter

---
+++ app/Console/Commands/ResetCounter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Counter;
+use Illuminate\Console\Command;
+
+class ResetCounter extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'counter:reset';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reset the JIRA task counter.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $counter = Counter::where('id', 1)->first();
+
+        if ($counter){
+            $counter->count = 0;
+            $counter->save();
+        }
+
+        return;
+    }
+}
