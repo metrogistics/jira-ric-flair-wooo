c7c5bfa34a4b4d4bdc631add230919f32282f89f
rwfwcb <rwfwcb@mail.missouri.edu>
2018-05-21 19:40:34 -0500

    updated migration

--- database/migrations/2018_05_21_223415_counter.php
+++ database/migrations/2018_05_21_223415_counter.php
@@ -14,6 +14,7 @@ class Counter extends Migration
     public function up()
     {
         Schema::create('counter', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('count');
         });
     }
